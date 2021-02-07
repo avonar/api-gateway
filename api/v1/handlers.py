a29bb22e9cbb39e0c4c2743fcc6a9becb63fdd1b
avonar <avonar@ozon.ru>
2021-02-07 17:47:38 +0300

    add statistics

--- api/v1/handlers.py
+++ api/v1/handlers.py
@@ -3,6 +3,34 @@ from aiohttp.web import Response
 from urllib.parse import urlparse
 from config.template import EXCLUDED_HEADERS
 import logging
+import functools
+
+stat_cache = {}
+
+
+def collect_statistics():
+    """
+    Return the statistics of REST api requests to external servers.
+    Example:
+    curl -X GET  "http://0.0.0.0:8080/statistics"
+    {'https://httpbin.org/anything': {'PUT': {'/': 2}, 'PATCH': {'/': 1}}}%
+    """
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrapped(*args):
+            if not stat_cache.get(args[0].headers['X-Target-Url']):
+                stat_cache[args[0].headers['X-Target-Url']] = {}
+            if not stat_cache[args[0].headers['X-Target-Url']].get(args[0].method):
+                stat_cache[args[0].headers['X-Target-Url']][args[0].method] = {}
+            if not stat_cache[args[0].headers['X-Target-Url']][args[0].method].get(args[0].path):
+                stat_cache[args[0].headers['X-Target-Url']][args[0].method][args[0].path] = 0
+            stat_cache[args[0].headers['X-Target-Url']][args[0].method][args[0].path] += 1
+            return await func(*args)
+
+        return wrapped
+
+    return wrapper
+
 
 def clear_headers(headers):
     if 'Host' in headers:
@@ -12,99 +40,81 @@ def clear_headers(headers):
             headers.pop(header)
     return headers
 
+
+@collect_statistics()
 async def handle_get(request: web.Request) -> Response:
     target_url = request.headers['X-Target-Url']
-    headers =  clear_headers(dict(request.headers))
-    path = request.path if request.path!='/' else ''
+    headers = clear_headers(dict(request.headers))
+    path = request.path if request.path != '/' else ''
     async with ClientSession() as session:
         async with session.get(f"{target_url}{path}", headers=headers) as resp:
             logging.info(resp.status)
-            headers =  clear_headers(dict(resp.headers))
+            headers = clear_headers(dict(resp.headers))
             await resp.text()
-            result = web.Response(
-                body=resp._body,
-                status=resp.status,
-                reason=resp.reason,
-                headers=headers
-            )
+            result = web.Response(body=resp._body, status=resp.status, reason=resp.reason, headers=headers)
             return result
 
 
-async def handle_post(request: web.Request)-> Response:
+@collect_statistics()
+async def handle_post(request: web.Request) -> Response:
     target_url = request.headers['X-Target-Url']
-    headers =  clear_headers(dict(request.headers))
+    headers = clear_headers(dict(request.headers))
     data = await request.json()
-    path = request.path if request.path!='/' else ''
+    path = request.path if request.path != '/' else ''
     async with ClientSession() as session:
         async with session.post(f"{target_url}{path}", headers=headers, data=data) as resp:
             logging.info(resp.status)
-            headers =  clear_headers(dict(resp.headers))
+            headers = clear_headers(dict(resp.headers))
             await resp.text()
-            result = web.Response(
-                body=resp._body,
-                status=resp.status,
-                reason=resp.reason,
-                headers=headers
-            )
+            result = web.Response(body=resp._body, status=resp.status, reason=resp.reason, headers=headers)
             return result
 
 
-
-async def handle_patch(request: web.Request)-> Response:
+@collect_statistics()
+async def handle_patch(request: web.Request) -> Response:
     target_url = request.headers['X-Target-Url']
-    headers =  clear_headers(dict(request.headers))
+    headers = clear_headers(dict(request.headers))
     data = await request.json()
-    path = request.path if request.path!='/' else ''
+    path = request.path if request.path != '/' else ''
     async with ClientSession() as session:
         async with session.patch(f"{target_url}{path}", headers=headers, data=data) as resp:
             logging.info(resp.status)
-            headers =  clear_headers(dict(resp.headers))
+            headers = clear_headers(dict(resp.headers))
             await resp.text()
-            result = web.Response(
-                body=resp._body,
-                status=resp.status,
-                reason=resp.reason,
-                headers=headers
-            )
+            result = web.Response(body=resp._body, status=resp.status, reason=resp.reason, headers=headers)
             return result
 
 
-
-async def handle_put(request: web.Request)-> Response:
+@collect_statistics()
+async def handle_put(request: web.Request) -> Response:
     target_url = request.headers['X-Target-Url']
-    headers =  clear_headers(dict(request.headers))
+    headers = clear_headers(dict(request.headers))
     data = await request.json()
-    path = request.path if request.path!='/' else ''
+    path = request.path if request.path != '/' else ''
     async with ClientSession() as session:
         async with session.put(f"{target_url}{path}", headers=headers, data=data) as resp:
             logging.info(resp.status)
-            headers =  clear_headers(dict(resp.headers))
+            headers = clear_headers(dict(resp.headers))
             await resp.text()
-            result = web.Response(
-                body=resp._body,
-                status=resp.status,
-                reason=resp.reason,
-                headers=headers
-            )
+            result = web.Response(body=resp._body, status=resp.status, reason=resp.reason, headers=headers)
             return result
 
 
-
-async def handle_delete(request: web.Request)-> Response:
+@collect_statistics()
+async def handle_delete(request: web.Request) -> Response:
     target_url = request.headers['X-Target-Url']
-    headers =  clear_headers(dict(request.headers))
+    headers = clear_headers(dict(request.headers))
     data = await request.json()
-    path = request.path if request.path!='/' else ''
+    path = request.path if request.path != '/' else ''
     async with ClientSession() as session:
         async with session.delete(f"{target_url}{path}", headers=headers, data=data) as resp:
             logging.info(resp.status)
-            headers =  clear_headers(dict(resp.headers))
+            headers = clear_headers(dict(resp.headers))
             await resp.text()
-            result = web.Response(
-                body=resp._body,
-                status=resp.status,
-                reason=resp.reason,
-                headers=headers
-            )
+            result = web.Response(body=resp._body, status=resp.status, reason=resp.reason, headers=headers)
             return result
 
+
+async def statistics(request: web.Request) -> Response:
+    result = web.Response(text=str(stat_cache))
+    return result
--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 # routes.py
 from aiohttp import web
 
-from api.v1.handlers import handle_get, handle_post, handle_patch, handle_put, handle_delete
+from api.v1.handlers import handle_get, handle_post, handle_patch, handle_put, handle_delete, statistics
 
 TARGET_URL = '/{tail:.*}'
 
 def setup_routes(app: web.Application) -> None:
+    app.router.add_route('GET', '/statistics', statistics)
     app.router.add_route('GET', TARGET_URL, handle_get)
     app.router.add_route('POST', TARGET_URL, handle_post)
     app.router.add_route('PATCH', TARGET_URL, handle_patch)
